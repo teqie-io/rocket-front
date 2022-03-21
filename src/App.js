a4e033f4012a133a6f650fbfb0642893cf08aa28
alish1999 <alishbhosale@gmail.com>
2022-03-21 18:07:33 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
-import './App.css';
 import Settings from "./Settings"
-import {Route,Routes} from "react-router-dom"
+import {Routes , Route} from "react-router-dom"
 
 function App() {
   return (
     
        <Routes>
-            <Route path="/" element={  <Settings/>  } />
+            <Route path='/' element={  <Settings/>  } />
+            <Route path='/a' element={  <Settings/>  } />
         </Routes>
   );
 }
--- src/Settings.js
+++ src/Settings.js
@@ -1,13 +1,14 @@
 //import logo from './logo.svg';
 import * as React from "react"
-export default function Settings()
+function Settings()
 {
     
     return(
         <nav className="navBar">
-        <div className="navBar--text">React - Blog</div>
-        <div className="navBar--welcome">Welcome</div>{/*show name only if isLogged is true */}
+        HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
         
          </nav>
     );
 }
+
+export default Settings;
