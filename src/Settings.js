9ceae6fc39d11beb19af6f0ec892215d99437db4
alish1999 <alishbhosale@gmail.com>
2022-03-21 17:58:31 +0530

    .

---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import * as React from "react"
+export default function Settings()
+{
+    
+    return(
+        <nav className="navBar">
+        <div className="navBar--text">React - Blog</div>
+        <div className="navBar--welcome">Welcome</div>{/*show name only if isLogged is true */}
+        
+         </nav>
+    );
+}
