697cc48d6c77f03c0cf1ff3500653d54838c47ef
alish1999 <alishbhosale@gmail.com>
2022-03-22 20:00:33 +0530

    .

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,12 +1,21 @@
 
 import "./SideBar.css";
-
+import "./logo192.png"
+import { Link } from "react-router-dom";
 export default function SideBar()
 {
     return(
         <div className="SideBar">
+            
             <h1 className="logo">LOGO</h1>
+            <ul className="optionlist">
+            
+            <li className="option"><image className="icon" src="./logo192.png"></image><Link className="option" to="#">Home</Link></li>
+            <li className="option"><image className="icon" src="./logo192.png"></image><Link className="option" to="#">Notifcations</Link></li>
+            <li className="option"><image className="icon" src="./logo192.png"></image><Link className="option" to="#">Configurations</Link></li>
+            <li className="option"><image className="icon" src="./logo192.png"></image><Link className="option" to="#">Subscribers</Link></li>
 
+            </ul>
         </div>
     )
 }
