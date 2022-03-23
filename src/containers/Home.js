e0eeb9d5e6b7ad74ed53e284604a15c72c60720e
alish1999 <alishbhosale@gmail.com>
2022-03-23 16:36:36 +0530

    Put recentnotifications component in other file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,76 +1,16 @@
 import React from "react";
 import { withTranslation } from 'react-i18next';
-import {Card} from "antd"
-import "./Home.scss";
-import { Row, Col } from 'antd';
+
+import RecentNotifications from "./RecentNotifications";
 
 class Home extends React.Component {
   render() {
     const {t} = this.props;
-    const notifications =[{
-      
-      id: 1,
-      content: "Hey dont miss this",
-      amount: "Rs 250",
-      value1: '24',
-      value2: '24'
-    },
-    {
-      
-      id: 2,
-      content: "Priya dont miss this ",
-      amount: "Rs 250",
-      value1: '23',
-      value2: '24'
-    },
-    {
-      
-      id: 3,
-      content:"Hey dont miss this",
-      amount: "Rs 250",
-      value1: '24',
-      value2: '24'
-    },
-    {
-      id: 4,
-      content:"Hey dont miss this",
-      amount: "Rs 250",
-      value1: '25',
-      value2: '24'
-    },
-    {
-      id: 5,
-      content:"Hey dont miss this",
-      state: false,
-      amount: "Rs 350",
-      value1: '26',
-      value2: '24'
-    },
- 
     
-  ]
     return (
       <div>
         <h1>Dashboard</h1>
-      <Card title={<div className="content1">Recent notifications</div>} className="notif-itm" 
-      style={{background:"#0D83DD"}}
-      bodyStyle={{background:"white"}}
-      
-      >
-        
-       
-       {notifications.map(item => (
-         <div>
-          <Row >
-          <Col span={7}>{item.content}</Col>
-          <Col span={6} >{item.amount}</Col>
-          <Col span={5} >{item.value1}</Col>
-          <Col span={5} >{item.value2}</Col>
-          </Row>
-         <hr/>
-         </div>
-       ))}
-      </Card>
+        <RecentNotifications/>
     </div>
     );
   }
---
+++ src/containers/RecentNotifications.js
@@ -0,0 +1,68 @@
+import React from "react"
+import {Card} from "antd"
+import "./RecentNotifications.scss";
+import { Row, Col } from 'antd';
+
+export default class RecentNotifications extends React.Component{
+    render(){
+        const notifications =[{
+      
+            id: 1,
+            content: "Hey dont miss this",
+            amount: "Rs 250",
+            value1: '24',
+            value2: '24'
+          },
+          {
+            
+            id: 2,
+            content: "Priya dont miss this ",
+            amount: "Rs 250",
+            value1: '23',
+            value2: '24'
+          },
+          {
+            
+            id: 3,
+            content:"Hey dont miss this",
+            amount: "Rs 250",
+            value1: '24',
+            value2: '24'
+          },
+          {
+            id: 4,
+            content:"Hey dont miss this",
+            amount: "Rs 250",
+            value1: '25',
+            value2: '24'
+          },
+          {
+            id: 5,
+            content:"Hey dont miss this",
+            state: false,
+            amount: "Rs 350",
+            value1: '26',
+            value2: '24'
+          },
+       
+          
+        ]
+        return(
+            <Card title={<div className="content1">Recent notifications</div>} className="notif-itm" 
+      style={{background:"#0D83DD"}}
+      bodyStyle={{background:"white"}}>
+       {notifications.map(item => (
+         <div>
+                <Row>
+                    <Col span={7}>{item.content}</Col>
+                    <Col span={6} >{item.amount}</Col>
+                    <Col span={5} >{item.value1}</Col>
+                    <Col span={5} >{item.value2}</Col>
+                </Row>
+                <hr/>
+         </div>
+       ))}
+      </Card>
+        )
+    }
+}
