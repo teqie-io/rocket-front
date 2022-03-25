2083386d12f080c63816afc80b6fc175eed61e8a
alish1999 <alishbhosale@gmail.com>
2022-03-25 17:37:37 +0530

    lower part of premium page

--- src/assets/CountIcons.js
+++ src/assets/CountIcons.js
@@ -3,9 +3,11 @@ import Icon from '@ant-design/icons';
 
 import { ReactComponent as SubscriberSVG } from './subscriber.svg'; 
 import { ReactComponent as SentMailSVG } from './sentmail.svg'; 
-import { ReactComponent as ImpressionsSVG } from './impressions.svg'; 
+import { ReactComponent as ImpressionsSVG } from './impressions.svg';
+import { ReactComponent as ChatSVG } from './chat.svg'; 
 
 
 export const SubscriberIcon = (props) => <Icon component={SubscriberSVG} {...props} />;
 export const SentMailIcon = (props) => <Icon component={SentMailSVG} {...props} />;
 export const ImpressionsIcon = (props) => <Icon component={ImpressionsSVG} {...props} />;
+export const ChatIcon = (props) => <Icon component={ChatSVG} {...props} />;
--- src/containers/Premium.js
+++ src/containers/Premium.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Row, Col } from 'antd';
 import {Card} from "antd"
 import  './premium.scss'
+import { ChatIcon } from "../assets/CountIcons";
 import TopLocations from "./home-components/TopLocations";
 import RecentNotifications from "./home-components/RecentNotifications";
 import TotalCounts from "./home-components/TotalCounts";
@@ -21,15 +22,19 @@ export default function Premium()
                 <Col span={8}><TopLocations/></Col>
                 <Col span={8}><RecentNotifications/></Col>
                 <Col span={8}><TopLocations/></Col>
+                <Col style={{padding:'30px'}}>**All plans come with unlimited subscribers</Col>
             </Row>
 
-            <Row justify="center">
+            <Row justify="center" align="middle" gutter={30}>
+                <Col span={19} offset={2}>
                 <Card title={<div className="prem-title">Current Plan</div>}
                 style={{background:"#0D83DD"}}
                 bodyStyle={{background:"#BDDCF3"}}
                 className="prem-card">
-                ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                   plan here
                 </Card>
+                </Col>
+                <Col span={3} ><ChatIcon onClick={()=>console.log("chat clicked")} style={{fontSize:'100px',cursor:"pointer"}}/></Col>
             </Row>
         </Card>
     )
