9b11235b25a7bde5f0d565973e67c6ac139f8396
alish1999 <alishbhosale@gmail.com>
2022-03-28 14:58:50 +0530

    styled buttons and components rendered from outside function

--- src/containers/Premium.js
+++ src/containers/Premium.js
@@ -3,57 +3,17 @@ import { Row, Col } from 'antd';
 import {Card} from "antd"
 import  './premium.scss'
 import { ChatIcon } from "../assets/CountIcons";
-import TopLocations from "./home-components/TopLocations";
 import { Radio } from 'antd';
 import {useState} from 'react';
 
 export default function Premium()
 {
-    const Basic=()=>{
-        return(
-            <Card title={
-                <h2 style={{'text-align':'center'}}>
-                    Basic
-                </h2>
-            } 
-            className="prem-card"
-            bodyStyle={{ padding:'0'}}>
-            <div>
-                <Row style={{'padding':'10px'}}>
-                    <Col span={24} align='middle'>Free</Col>
-                    <Col span={24} align='middle'>Lifetime</Col>
-                </Row>
-                <Row style={{'padding':'10px','background':'#F3FBFF'}}>
-                    <Col span={24} align='middle'>500</Col>
-                    <Col span={24} align='middle'>Impressions per month</Col>
-                </Row>
-                <Row style={{'padding':'20px'}}> 
-                    <Col span={24} offset={4}>
-                        <ul>
-                            <li>Unlimited Subscribers</li>
-                            <li>Send & Schedule Campaigns</li>
-                            <li>Back-In-Stock Automation</li>
-                            <li>Price Drop Automation</li>
-                            <li>Basic Reports</li>
-                            <li>Chat Support</li>
-                        </ul>
-                    </Col>
-                </Row>
-                <Row style={{'padding':'20px'}}>
-                    <Col span={24} align='middle'>
-                        <button>Start Trial</button>
-                    </Col>
-                </Row>
-            </div>
-            </Card>
-        )   
-    }
     const plantypes = [
         { label: 'MONTHLY', value: 'monthly' },
         { label: 'Yearly', value: 'yearly' },
       ];
-      const [optionval,setState] =useState('monthly')
-      function onChange2(e){
+    const [optionval,setState] =useState('monthly')
+    function onChange2(e){
         console.log(e.target.value);
         setState(e.target.value);
       };
@@ -72,7 +32,7 @@ export default function Premium()
 
             <Row justify="center" gutter={50} align="bottom" style={{padding:'30px'}}>
                 <Col span={8}><Basic/></Col>
-                <Col span={8}><TopLocations/></Col>
+                <Col span={8}><Basic/></Col>
                 <Col span={8}><Basic/></Col>
                 <Col style={{padding:'30px'}}>**All plans come with unlimited subscribers</Col>
             </Row>
@@ -90,4 +50,44 @@ export default function Premium()
             </Row>
         </Card>
     )
+}
+
+const Basic=()=>{
+    return(
+        <Card title={
+            <h2 style={{'text-align':'center'}}>
+                Basic
+            </h2>
+        } 
+        className="prem-card"
+        bodyStyle={{ padding:'0'}}>
+        <div>
+            <Row style={{'padding':'10px'}}>
+                <Col span={24} align='middle'>Free</Col>
+                <Col span={24} align='middle'>Lifetime</Col>
+            </Row>
+            <Row style={{'padding':'10px','background':'#F3FBFF'}}>
+                <Col span={24} align='middle'>500</Col>
+                <Col span={24} align='middle'>Impressions per month</Col>
+            </Row>
+            <Row style={{'padding':'20px'}}> 
+                <Col span={24} offset={4}>
+                    <ul className="ul1">
+                        <li>Unlimited Subscribers</li>
+                        <li>Send & Schedule Campaigns</li>
+                        <li>Back-In-Stock Automation</li>
+                        <li>Price Drop Automation</li>
+                        <li>Basic Reports</li>
+                        <li>Chat Support</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row style={{'padding':'20px'}}>
+                <Col span={24} align='middle'>
+                    <button className="prem-button">Start Trial</button>
+                </Col>
+            </Row>
+        </div>
+        </Card>
+    )   
 }
