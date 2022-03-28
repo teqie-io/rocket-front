5ea6e9823c77ee3b99d53efd1d54fd6394cfeec2
alish1999 <alishbhosale@gmail.com>
2022-03-28 16:11:21 +0530

    Added business plan

--- src/containers/Premium.js
+++ src/containers/Premium.js
@@ -12,7 +12,7 @@ export default function Premium()
         { label: 'MONTHLY', value: 'monthly' },
         { label: 'Yearly', value: 'yearly' },
       ];
-    const [optionval,setState] =useState('monthly')
+    const [optionVal,setState] =useState('monthly')
     function onChange2(e){
         console.log(e.target.value);
         setState(e.target.value);
@@ -25,21 +25,21 @@ export default function Premium()
                         <Col span={24} align='middle'><div style={{'fontSize':'36px','padding-bottom':'20px'}}>Pick a plan</div></Col>
                     </Row>
                     <Row>
-                    <Radio.Group size="large" options={plantypes} onChange={onChange2} value={optionval}  optionType="button" buttonStyle="solid" />
+                        <Radio.Group size="large" options={plantypes} onChange={onChange2} value={optionVal}  optionType="button" buttonStyle="solid" />
                     </Row>
                 </Card>
             </Row>
 
-            <Row justify="center" gutter={50} align="bottom" style={{padding:'30px'}}>
-                <Col span={8}><Basic/></Col>
-                <Col span={8}><Basic/></Col>
-                <Col span={8}><Basic/></Col>
+            <Row justify="center" gutter={80} align="bottom" style={{padding:'30px'}}>
+                <Col span={8}> <Basic/>      </Col>
+                <Col span={8}> <Business/>      </Col>
+                <Col span={8}> <Enterprise/> </Col>
                 <Col style={{padding:'30px'}}>**All plans come with unlimited subscribers</Col>
             </Row>
 
             <Row justify="center" align="middle" gutter={30}>
                 <Col span={19} offset={2}>
-                <Card title={<div className="prem-title">Current Plan</div>}
+                <Card title={<div className="prem-title" style={{'color':'white'}}>Current Plan</div>}
                 style={{background:"#0D83DD"}}
                 bodyStyle={{background:"#BDDCF3"}}
                 className="prem-card">
@@ -55,19 +55,19 @@ export default function Premium()
 const Basic=()=>{
     return(
         <Card title={
-            <h2 style={{'text-align':'center'}}>
+            <div className="prem-title">
                 Basic
-            </h2>
+            </div>
         } 
         className="prem-card"
         bodyStyle={{ padding:'0'}}>
         <div>
-            <Row style={{'padding':'10px'}}>
-                <Col span={24} align='middle'>Free</Col>
+            <Row style={{'padding':'20px'}}>
+                <Col span={24} align='middle' className="prem-blue-text">Free</Col>
                 <Col span={24} align='middle'>Lifetime</Col>
             </Row>
             <Row style={{'padding':'10px','background':'#F3FBFF'}}>
-                <Col span={24} align='middle'>500</Col>
+                <Col span={24} align='middle' className="prem-blue-text">500</Col>
                 <Col span={24} align='middle'>Impressions per month</Col>
             </Row>
             <Row style={{'padding':'20px'}}> 
@@ -79,6 +79,86 @@ const Basic=()=>{
                         <li>Price Drop Automation</li>
                         <li>Basic Reports</li>
                         <li>Chat Support</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row style={{'padding':'20px'}}>
+                <Col span={24} align='middle'>
+                    <button className="prem-button">Start Trial</button>
+                </Col>
+            </Row>
+        </div>
+        </Card>
+    )   
+}
+
+const Business= () =>{
+    return(
+        <Card title={
+            <div className="prem-title" style={{'color':'white','padding-top':'15px','padding-bottom':'15px'}}>
+                Business
+            </div>
+        } 
+        className="prem-card"
+        style={{background:"#0D83DD"}}
+        bodyStyle={{background:"white",padding:'0'}}>
+        <div>
+            <Row style={{'padding':'35px'}}>
+                <Col span={24} align='middle' className="prem-blue-text">30$</Col>
+                <Col span={24} align='middle'>only $30/mo on annual plan</Col>
+            </Row>
+            <Row style={{'padding':'10px','background':'#F3FBFF'}}>
+                <Col span={24} align='middle' className="prem-blue-text">500</Col>
+                <Col span={24} align='middle'>Impressions per month</Col>
+            </Row>
+            <Row style={{'padding':'20px'}}> 
+                <Col span={24} offset={4}>
+                    <ul className="ul1">
+                        <li>All Basic Features </li>
+                        <li>Abandoned Cart Automation</li>
+                        <li>Shipping Notification</li>
+                        <li>Hero Image Support</li>
+                        <li>Subscriber Details</li>
+                        <li>Subscriber Import And Export</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row style={{'padding':'20px'}}>
+                <Col span={24} align='middle'>
+                    <button className="prem-button">Start Trial</button>
+                </Col>
+            </Row>
+        </div>
+        </Card>
+    )   
+}
+
+const Enterprise = () =>{
+    return(
+        <Card title={
+            <div className="prem-title">
+                Enterprise
+            </div>
+        } 
+        className="prem-card"
+        bodyStyle={{ padding:'0'}}>
+        <div>
+            <Row style={{'padding':'23px'}}>
+                <Col span={24} align='middle' className="prem-blue-text">Get in touch with us</Col>
+             
+            </Row>
+            <Row style={{'padding':'20px'}}> 
+                <Col span={24} offset={4}>
+                    <ul className="ul1">
+                        <li>All Business Features </li>
+                        <li>Segmentation</li>
+                        <li>Flash Sale</li>
+                        <li>Smart Delivery</li>
+                        <li>Browse Abandonment</li>
+                        <li>Strategy Manager</li>
+                        <li>Integration Engineer</li>
+                        <li>Custom buttons</li>
+                        <li>Phone Support</li>
                     </ul>
                 </Col>
             </Row>
