15717c03dce4b8b58da454edd8756f4cc0b324d9
alish1999 <alishbhosale@gmail.com>
2022-03-30 16:17:48 +0530

    tab and contents in flow

--- src/containers/notifications/abandoned/AbandonedCart.js
+++ src/containers/notifications/abandoned/AbandonedCart.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next';
-import { Card, Tabs,Button } from 'antd';
+import { Card, Tabs,Button,Row,Col } from 'antd';
 import {Link} from "react-router-dom"
-
+import { Switch } from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 
 function AbandonedCart()
 {
@@ -11,13 +12,23 @@ function AbandonedCart()
         console.log(key);
       }
     return(
-        <Card title="Abandoned Cart Recovery">
-        <Button>
-            <Link to='/notifications/abandoned'>EDIT</Link> 
-        </Button> 
-        <Tabs defaultActiveKey="1" onChange={callback}>
+        <Card title={<div style={{'fontWeight':"500",'fontSize':'32px'}}>Abandoned Cart Recovery</div>}>
+       
+        <Tabs size='large' defaultActiveKey="1" onChange={callback}>
             <TabPane tab="Flow" key="1">
-            Content of Tab Pane 1
+                <Card>
+                    <Button>
+                        <Link to='/notifications/abandoned'>TO EDIT</Link> 
+                    </Button> 
+                    
+                    <Row span={12}><Col span={18}> Content of Tab Pane 1</Col>
+
+                    <Switch size='30px' style={{'height':'30px','width':'60px'}}
+                    checkedChildren={<CheckOutlined style={{'fontSize':'15px'}}  />}
+                    unCheckedChildren={<CloseOutlined style={{'fontSize':'15px'}} />}
+                    defaultChecked
+                    /></Row>
+                </Card>
             </TabPane>
             <TabPane tab="Report" key="2">
             Content of Tab Pane 2
