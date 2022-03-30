3113fa6734ec49b911374d82f5a2b22bb45a1576
alish1999 <alishbhosale@gmail.com>
2022-03-30 19:17:58 +0530

    Report structure

--- src/containers/notifications/abandoned/AbandonedCart.jsx
+++ src/containers/notifications/abandoned/AbandonedCart.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next';
-import { Card, Tabs,Button,Row,Col } from 'antd';
+import { Card, Tabs,Button,Row,Col,Select } from 'antd';
 import {Link} from "react-router-dom"
 import { Switch } from 'antd';
 import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+
+
+const { Option } = Select;
 
 function AbandonedCart()
 {
@@ -11,10 +14,11 @@ function AbandonedCart()
     function callback(key) {
         console.log(key);
       }
+
     return(
         <Card title={<div style={{'fontWeight':"500",'fontSize':'32px'}}>Abandoned Cart Recovery</div>}>
        
-        <Tabs size='large' defaultActiveKey="1" onChange={callback}>
+        <Tabs size='large' defaultActiveKey="2" onChange={callback}>
             <TabPane tab="Flow" key="1">
                 <Card>
                     <Button>
@@ -22,16 +26,16 @@ function AbandonedCart()
                     </Button> 
                     
                     <Row span={12}><Col span={18}> Content of Tab Pane 1</Col>
-
-                    <Switch size='30px' style={{'height':'30px','width':'60px'}}
-                    checkedChildren={<CheckOutlined style={{'fontSize':'15px'}}  />}
-                    unCheckedChildren={<CloseOutlined style={{'fontSize':'15px'}} />}
-                    defaultChecked
-                    /></Row>
+                        <Switch size='30px' style={{'height':'30px','width':'60px'}}
+                        checkedChildren={<CheckOutlined style={{'fontSize':'15px'}}  />}
+                        unCheckedChildren={<CloseOutlined style={{'fontSize':'15px'}} />}
+                        defaultChecked
+                    />
+                    </Row>
                 </Card>
             </TabPane>
             <TabPane tab="Report" key="2">
-            Content of Tab Pane 2
+                <CartReport/>
             </TabPane>
         </Tabs>
           
@@ -40,3 +44,63 @@ function AbandonedCart()
 }
 
 export default withTranslation()(AbandonedCart);
+
+
+const CartReport = () => {
+    function handleChange(value) {
+        console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+      }
+    return(
+        <Card>
+            {/* In queue and revenue */}
+            <Col span={6}>
+                <Card className='home-card'>
+                    <Row  gutter={100}>
+                        <Col span={12}>
+                            <Row align="middle">
+                                <Col align="middle" span={24}><h2 className="count-text">3</h2></Col>                      
+                            </Row>
+                            <Row>
+                                <Col  align="middle" span={24}>In queue</Col>
+                            </Row>
+                        </Col>
+                        <Col span={12}>
+                            <Row gutter={20} align="middle">
+                            <Col align="middle" span={24}><h2 className="count-text">2k</h2></Col>                      
+                            </Row>
+                            <Row>
+                                <Col align="middle" span={24}>Revenue</Col>
+                            </Row>
+                        </Col>
+                     </Row>
+                </Card>
+            </Col>
+            {/* Status */}
+            <Row gutter={50}>
+                <Col span={12}>
+                    <Card>
+                        <h2>
+                            Status
+                        </h2>
+                        <Select size='large'
+                        labelInValue
+                        defaultValue={{ value: 'weekly' }}
+                        style={{ width: 200 }}
+                        onChange={handleChange}
+                        >
+                            <Option value="weekly">By week</Option>
+                            <Option value="monthly">By month</Option>
+                            <Option value="yearly">By year</Option>
+                        </Select>
+                    </Card>    
+                </Col>
+                {/* Recovery Rates */}
+                <Col span={12}>Abandoned Recovery Rate</Col>
+           
+            </Row>
+            {/* Sales */}
+
+        </Card>
+
+    )
+}
