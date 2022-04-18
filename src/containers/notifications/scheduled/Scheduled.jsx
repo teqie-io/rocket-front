39d055966331ba27d42b7a8ffb2d5e554726d129
alish1999 <alishbhosale@gmail.com>
2022-04-18 13:47:42 +0530

    Scheduled notifications list

--- src/containers/notifications/scheduled/Scheduled.jsx
+++ src/containers/notifications/scheduled/Scheduled.jsx
@@ -3,6 +3,7 @@ import { withTranslation } from 'react-i18next';
 import { Card, Tabs,Row,Col,Select,Progress,List,Timeline} from 'antd';
 //import {ResponsiveContainer} from 'recharts';
 import {Link} from "react-router-dom"
+import {EditOutlined} from '@ant-design/icons';
 
 import {useState,useEffect} from 'react';
 
@@ -86,6 +87,35 @@ function Scheduled()
             revenue:'2k'
         },
     ]
+    const schedulednotifications=[
+        {
+            title:'Grab the offer now, 10% Off',
+            subscribers:'All Subscribers',
+            date:'9 days ago',
+            campaignID:'360742',
+            time:'6 : 00 PM',
+            date:'24th March',
+            isActive:true
+        },
+        {
+            title:'Grab the offer now, 10% Off',
+            subscribers:'All Subscribers',
+            date:'9 days ago',
+            campaignID:'360742',
+            time:'6 : 00 PM',
+            date:'24th March',
+            isActive:false
+        },
+        {
+            title:'Grab the offer now, 10% Off',
+            subscribers:'All Subscribers',
+            date:'9 days ago',
+            campaignID:'360742',
+            time:'6 : 00 PM',
+            date:'24th March',
+            isActive:true
+        },
+    ]
 
     function colorswitcher(index)
     {
@@ -130,7 +160,34 @@ function Scheduled()
             </TabPane>
             {/* TAB 2 Scheduled */}
             <TabPane tab={<div style={{'fontSize':'24px'}}>Scheduled</div>} key="2">
-               scheduled
+                <List style={{'paddingTop':'0px'}}>
+                            {
+                            schedulednotifications.map((item,index) => (
+                                <List.Item key={item.campaignID} style={{'paddingTop':'20px',background:`${colorswitcher(index)}`}}> 
+                                            <Col span={8} style={{padding:'20px','paddingLeft':'40px'}}>
+                                               <Row style={{'fontSize':'24px'}}>{item.title}</Row>
+                                               <Row style={{'fontSize':'15px'}}>{item.subscribers} | {item.date} | Campaign ID:{item.campaignID}</Row> 
+                                            </Col>
+                                            <Col span={4}>
+                                                <Row justify='center' style={{'fontSize':'16px'}}>Time</Row>
+                                                <Row justify='center' className='prem-blue-text' style={{'fontSize':'18px'}}>{item.time}</Row>
+                                            </Col>
+                                            <Col span={4}>
+                                                <Row justify='center' style={{'fontSize':'16px'}}>Date</Row>
+                                                <Row justify='center' className='prem-blue-text' style={{'fontSize':'18px'}}>{item.date}</Row>
+                                            </Col>
+                                            <Col span={4}>
+                                                <Row justify='center' style={{'fontSize':'16px'}}>Status</Row>
+                                                <Row justify='center' className='prem-blue-text'  style={{'fontSize':'18px',color:'#01A308'}}>
+                                                    {item.isActive && `Active`}
+                                                    {item.isActive || `Inactive`}
+                                                </Row>
+                                            </Col>
+                                            <Col span={4}>
+                                                <Row justify='center' style={{'fontSize':'24px'}}><EditOutlined/></Row>                                            </Col>
+                                </List.Item>
+                            ))}
+                    </List>
             </TabPane>
         </Tabs>
           
